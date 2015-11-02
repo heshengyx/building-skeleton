6d8b90bff7fa942f4e08e2e769339882e3dec712
hes <heshengyx@qq.com>
2015-11-02 14:48:41 +0800

    1

--- src/main/java/com/house/building/page/Page.java
+++ src/main/java/com/house/building/page/Page.java
@@ -1,12 +1,77 @@
 package com.house.building.page;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
-public interface Page<T> extends java.io.Serializable {
+/**
+ * 描述：
+ * 
+ * <pre>
+ * HISTORY
+ * ****************************************************************************
+ *  ID   DATE           PERSON          REASON
+ *  1    2015年11月2日      608279         Create
+ * ****************************************************************************
+ * </pre>
+ * 
+ * @author 608279
+ * @since 1.0
+ */
+public class Page<T> implements IPage<T> {
 
-	int getTotal();
-	int getSize();
-	int getIndex();
-	int getTotalRecord();
-	Collection<T> getData();
+	/**  */
+	private static final long serialVersionUID = 4336812407961532349L;
+
+	// the record in page
+	private int size;
+
+	// page index
+	private int index;
+
+	// the record total
+	private int total;
+
+	private int totalRecord;
+
+	private Collection<T> data;
+
+	public Page() {
+	}
+
+	public Page(Collection<T> data, int totalRecord, int index, int size) {
+		this.data = (data == null ? new ArrayList<T>(0) : data);
+		this.totalRecord = totalRecord;
+		this.size = size;
+		this.index = index;
+		this.total = (totalRecord - 1) / size + 1;
+	}
+
+	public void setTotalRecord(int totalRecord) {
+		this.totalRecord = totalRecord;
+	}
+
+	public int getTotalRecord() {
+		return totalRecord;
+	}
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public int getTotal() {
+		return total;
+	}
+
+	public int getIndex() {
+		return index;
+	}
+
+	public Collection<T> getData() {
+		return data;
+	}
+
 }
